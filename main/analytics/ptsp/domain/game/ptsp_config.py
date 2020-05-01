7bc2973c90bd627af7f2f4a7df737cc54b999cea
Matmozaur <matt950806@wp.pl>
2020-05-01 22:56:24 +0200

    api ptsp first template

---
+++ main/analytics/ptsp/domain/game/ptsp_config.py
@@ -0,0 +1,7 @@
+class PTSPConfiguration:
+
+    def __init__(self, dt=0.1, alpha=0.1, acc=1.0, slow=0.1):
+        self.dt = dt
+        self.alpha = alpha
+        self.acc = acc
+        self.slow = slow
